267eb8d32900bd7c2d27035de735b97985910cef
Khanchouch-Ahmed <63616721+Khanchouch-Ahmed@users.noreply.github.com>
2021-03-19 10:43:59 +0100

    version service

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
-
+version = '1.0.0'
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 @app.route('/french')
 def french_hello_world():
     return 'Bonjouuuugh'
+@app.route('/version'):
+def get_version():
+    return version
